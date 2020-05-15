be0a1ed261509cd3eb9a1faa635e3343827116ec
rickytorrez <torrez.ricky@ymail.com>
2020-05-14 22:27:14 -0400

    geocoder, pagination, seeder

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,15 +1,87 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  // copy req.query
+  const reqQuery = { ...req.query };
+
+  // fields to exclude - don't want to be matched on filtering
+  const removeFields = ['select', 'sort', 'page', 'limit'];
+
+  // loop over removeFields and delete select from the reQuery parameters
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  // create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // create operators ($gt, $gte, etc)
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+
+  // finding resource
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  // select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // pagination - comes in as a string, we want an integer
+  const page = parseInt(req.query.page, 10) || 1;
+  // 10 resources per page
+  const limit = parseInt(req.query.limit, 10) || 25;
+  // startIndex
+  const startIndex = (page - 1) * limit;
+  // endIndex
+  const endIndex = page * limit;
+  // total amount of resorces
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
+  // executing query
+  const bootcamps = await query;
+
+  // pagination result
+  const pagination = {};
+
+  // if we don't have a previous or last page, don't show them
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit: limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit: limit,
+    };
+  }
+
   res.status(200).json({
     success: true,
     count: bootcamps.length,
+    pagination: pagination,
     data: bootcamps,
   });
 });
@@ -74,5 +146,33 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     data: {},
+  });
+});
+
+// @desc    Get bootcamps within a radius
+// @route   GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  Public
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  // req.params are coming from the url
+  const { zipcode, distance } = req.params;
+
+  // get lat/long from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // calculate the radius using radians
+  // divide distance by radius of eartch
+  // earh radius = 3,963 mi || 6,378 km
+  const radius = distance / 3963;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
   });
 });
---
+++ seeder.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// load env variables
+dotenv.config({ path: './config/config.env' });
+
+// load the models
+const Bootcamp = require('./models/Bootcamp');
+
+// connect to database
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+// read JSON files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+// import data into database
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log('Data Imported...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// delete data from database
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log('Data Destroyed'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
